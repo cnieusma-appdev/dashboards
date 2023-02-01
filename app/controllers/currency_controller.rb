269d3961de8612e9577a135168945fcf61a1c4a4
cnieusma <cnieusma@uchicago.edu>
2023-02-01 21:33:32 +0000

    fix conversion

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -27,11 +27,15 @@ end
 
 def final_conversion
   @b_currency = params.fetch("base_currency")
-  @raw_data = open("https://api.exchangerate.host/symbols").read
-  @parsed_data = JSON.parse(@raw_data)
-  @symbols_hash = @parsed_data.fetch("symbols")
-  @array_of_symbols = @symbols_hash.keys
+  @t_currency = params.fetch("translated_currency")
   
+  url = "https://api.exchangerate.host/convert?from=" + @b_currency + "&to=" + @t_currency
+
+  @raw_data = open(url).read
+  @parsed_data = JSON.parse(@raw_data)
+  @result = @parsed_data.fetch("result")
+
+
   render({ :template => "layouts/final_conversion.html.erb"})
 
 end
